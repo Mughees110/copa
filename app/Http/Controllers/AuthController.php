1b8318c00d672de613cd3604aa3a819dd73a8a90
Mughees110 <zaidimughees@gmail.com>
2024-07-31 20:28:54 +0500

    ds

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -222,6 +222,17 @@ class AuthController extends Controller
             ], 422);
         }
     }
+    public function employLogin(Request $request){
+        $userE=User::where('employId',$request->json('employId'))->where('clubId',$request->json('clubId'))->where('password',$request->json('password'))->exists();
+        if($userE==false){
+            return response()->json([
+                    'message' => 'The credentials are invalid',
+                ], 422);
+        }
+        $user=User::where('employId',$request->json('employId'))->where('clubId',$request->json('clubId'))->where('password',$request->json('password'))->first();
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json(['status'=>200,'token'=>$token,'data'=>$user]);
+    }
     
 
 }
--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ Route::post('register','App\Http\Controllers\AuthController@register');
 //Route::post('gmail','App\Http\Controllers\AuthController@gmail');
 Route::post('login','App\Http\Controllers\AuthController@login');
 Route::post('login2','App\Http\Controllers\AuthController@login2');
+Route::post('employ-login','App\Http\Controllers\AuthController@employLogin');
 Route::post('user-exists','App\Http\Controllers\AuthController@userExists');
 
 Route::get('invalid',function(){

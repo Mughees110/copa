b327bd652d15cd2a4838248360b201bd9e9399f6
Mughees110 <zaidimughees@gmail.com>
2024-09-20 04:36:44 +0500

    ds

--- config/cors.php
+++ config/cors.php
@@ -1,34 +1,20 @@
 <?php
-
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
+    'paths' => ['api/*', 'csrf-token'], // Adjust to include your CSRF endpoint
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'], // You can specify specific methods like ['GET', 'POST']
 
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:3000'], // Allow requests from your React app
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['*'], // You can specify specific headers if needed
 
     'exposed_headers' => [],
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true, // Set to true if you need to send cookies or HTTP authentication
 
 ];
